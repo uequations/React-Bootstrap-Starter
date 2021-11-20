e624c34cad4f47def6cb0426e02320d2726defda
Mensah Alkebu-Lan <malkebu-lan@uequations.com>
2021-11-19 23:07:29 -0500

    added button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
 
 import './App.css';
 
-const App = () => (<Container/>);
+const App = () => (
+    <Container>
+        <Button variant="primary">Click Me!</Button>
+    </Container>
+);
 
 export default App;
